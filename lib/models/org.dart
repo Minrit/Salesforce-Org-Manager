cdd4e09093e368198bad93f7824322d0be89f001
Shangmin Dou <shangmindou@gmail.com>
2022-06-20 17:05:23 +0800

    feat(app): update the model and add json serialization.

--- lib/models/org.dart
+++ lib/models/org.dart
@@ -1,5 +1,10 @@
 import 'user.dart';
 
+import 'package:json_annotation/json_annotation.dart';
+
+part 'org.g.dart';
+
+@JsonSerializable()
 class Org {
   String name;
   String? domain;
@@ -8,4 +13,7 @@ class Org {
   List<User>? userList = [];
 
   Org(this.name, this.isProduction, {this.domain, this.comment, this.userList});
+  factory Org.fromJson(Map<String, dynamic> json) => _$OrgFromJson(json);
+  Map<String, dynamic> toJson() => _$OrgToJson(this);
+
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,6 +1,12 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'user.g.dart';
+@JsonSerializable()
 class User {
   late String username;
   late String password;
 
   User(this.username, this.password);
+
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
